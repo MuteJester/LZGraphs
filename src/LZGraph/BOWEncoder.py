421926a1447566f14efb96f617baacbfece646f4
Thomas Konstantinovsky <56339082+MuteJester@users.noreply.github.com>
2022-09-26 13:47:52 +0300

    Adde BowVectorizer

---
+++ src/LZGraph/BOWEncoder.py
@@ -0,0 +1,69 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from tqdm.auto import tqdm
+
+from src.LZGraph import lempel_ziv_decomposition
+
+
+class BOWVectorizer:
+    """
+       A Bag of words vectorizer, can be fitted on a list of repertoires and used
+       to output LZ-BOW representation
+
+       ...
+
+       Methods
+       -------
+       fit(list_of_repertoires):
+           fits the vectorizer model to the dictionary derived from the repertoires given by the argument
+           "list_of_repertoires"
+
+       transform(list_of_repertoires):
+            given a list of repertoires the function will use the fitted BOW dictionary to return
+            the bag of words vectors for each repertoire in the list
+
+       """
+    def __init__(self):
+
+        self.vectorizer = CountVectorizer(lowercase=False, token_pattern=r'(?u)\b\w+\b', ngram_range=(1, 1))
+
+
+    def fit(self,list_of_repertoires):
+
+        """
+           fits the BOW dictionary based on the repertoires given in "list_of_repertoires"
+
+                   Parameters:
+                           list_of_repertoires (list): A list of pandas DataFrame's that have a column named "cdr3_rearrangement"
+
+                   Returns:
+                           None
+        """
+
+        cmp = []
+        for d in tqdm(list_of_repertoires,leave=False):
+            for cdr3 in d['cdr3_rearrangement']:
+                cmp.append(' '.join(lempel_ziv_decomposition(cdr3)))
+        self.vectorizer.fit(cmp)
+
+    def transform(self,list_of_repertoires):
+        """
+              transforms a list of repertoires into a list of bag of words vectors derived based on fitted repertoires
+
+                      Parameters:
+                              list_of_repertoires (list): A list of pandas DataFrame's that have a column named "cdr3_rearrangement"
+
+                      Returns:
+                              None
+       """
+        n_encoded = []
+        for d in tqdm(list_of_repertoires):
+            cmp = []
+            for cdr3 in d['cdr3_rearrangement']:
+                cmp.append(' '.join(lempel_ziv_decomposition(cdr3)))
+            n_encoded.append(self.vectorizer.transform([' '.join(cmp)]).todense())
+        n_encoded = np.array(n_encoded)
+        n_encoded = n_encoded.squeeze()
+        return n_encoded
+
+
--- src/LZGraph/__init__.py
+++ src/LZGraph/__init__.py
@@ -1,3 +1,9 @@
 __version__ = "0.1.0"
 
 from .decomposition import *
+import src.LZGraph.NaiveLZGraph
+import src.LZGraph.AAPLZGraph
+import src.LZGraph.BOWEncoder
+import src.LZGraph.NDPLZGraph
+import src.LZGraph.misc
+
